114b521d3c858a9d2ed4e4a3f7412bbbba802ac1
Prapti Jaduvanshi <khushi.jaduvanshi@gmail.com>
2024-02-11 22:16:47 +0530

    Draft of home page and content

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const phrases = [
+  "No",
+  "Are you sure?",
+  "Really sure?",
+]
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed]= useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  function handleNoClick() {
+    setNoCount(noCount + 1);
+  }
+
+  function getNoButtonText() {
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='valentine-container'>
+      { yesPressed ? (
+        <>
+        <img alt='bears kissing' src='https://media.tenor.com/sFStC1YwBzMAAAAi/milk-and-mocha-hug.gif'/>
+        <div className='text'> Yaye!!</div>
+        </>
+      ) : (
+        <>
+        <img alt='bears with hearts' src='https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.webp' />
+        <div> Will you be my Valentine? </div>
+        <div>
+          <button onClick={() => setYesPressed(true)} style={{fontSize: yesButtonSize}}>Yes</button>
+          <button onClick={handleNoClick}>{getNoButtonText()}</button>
+        </div>
+        </> 
+      )}
+
+    </div>
   )
 }
 
